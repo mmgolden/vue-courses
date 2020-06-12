f2eca47f78568467fbb79cd5f8eb403e635ff951
Melinda Golden <info@melindagolden.com>
2020-06-12 16:42:43 -0400

    add: normalize.css

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+/* Import main.scss */
+import './assets/scss/main.scss';
 
 Vue.config.productionTip = false;
 
@@ -9,4 +12,4 @@ new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app');
