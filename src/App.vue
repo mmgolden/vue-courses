67e6f600e7a286ba82fecd969f531cc729dd4ff6
Melinda Golden <info@melindagolden.com>
2020-06-12 18:04:01 -0400

    add: Navigation to App

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <template>
   <div id="app">
-    <router-view />
+    <navigation></navigation>
+    <main class="main-container">
+      <router-view />
+    </main>
   </div>
 </template>
 
-<style lang="scss"></style>
+<script>
+import Navigation from '@/components/Navigation.vue';
+
+export default {
+  components: {
+    Navigation
+  }
+};
+</script>
+
+<style lang="scss">
+.main-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 1.875rem;
+}
+</style>
