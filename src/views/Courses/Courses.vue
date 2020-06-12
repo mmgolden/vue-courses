d8959262cc792e7316d1284eb90803d399166eff
Melinda Golden <info@melindagolden.com>
2020-06-12 18:05:05 -0400

    add: Container styling to Courses

--- src/views/Courses/Courses.vue
+++ src/views/Courses/Courses.vue
@@ -1,9 +1,17 @@
 <template>
-  <h1>Courses</h1>
+  <div class="courses-container"><h1>Courses</h1></div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.courses-container {
+  width: 100%;
+
+  @media screen and (min-width: 992px) {
+    max-width: 1130px;
+  }
+}
+</style>
