9cbfd350f82310d8eae550a857bd2fbfb86dd20b
Melinda Golden <info@melindagolden.com>
2020-06-12 22:04:20 -0400

    add: login and signup to routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,31 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Courses from '../views/Courses/Courses';
+import Courses from '../views/Courses.vue';
 
 Vue.use(VueRouter);
 
+const Login = () =>
+  import(/* webpackChunkName: "login" */ '../views/Login.vue');
+
+const Signup = () =>
+  import(/* webpackChunkName: "signup" */ '../views/Signup.vue');
+
 const routes = [
+  { path: '/', redirect: '/courses' },
   {
-    path: '/',
-    name: 'Courses',
+    path: '/courses',
+    name: 'courses',
     component: Courses
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: Signup
   }
 ];
 
