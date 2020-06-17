285f83a0c0510ef1e35b33c3a56483bca36a8ecd
Melinda Golden <info@melindagolden.com>
2020-06-16 22:12:54 -0400

    add: CourseTabs

---
+++ src/components/CourseTabs.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul class="course-tabs">
+    <li>All courses</li>
+    <li>Favorites</li>
+  </ul>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.course-tabs {
+  list-style: none;
+  padding: 0;
+  margin: 0 0 2rem 0;
+  display: flex;
+  border-bottom: 1px solid $color-divider;
+
+  li {
+    padding: 1rem 1.5rem;
+  }
+}
+</style>
